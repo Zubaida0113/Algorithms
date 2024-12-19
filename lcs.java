227683739be3825f7f6791dc03e0f37557722201
Zubaida0113 <zubaidasehnaz@gmail.com>
2024-12-19 20:40:02 +0530

    Update lcs.java

--- lcs.java
+++ lcs.java
@@ -26,7 +26,7 @@ class lcs{
             return 0;
         }
         if(dp[n][m]!= -1){
-            return dp[n-1][m-1];
+            return dp[n][m];
         }
         if(s1.charAt(n-1)==s2.charAt(m-1)){
            return dp[n][m]=1+LCSdynamic(s1,s2,n-1,m-1,dp);
@@ -36,14 +36,20 @@ class lcs{
         }
     }
     public static void main(String[] args) {
-        String s1="A";
-        String s2="ACD";
+        String s1="ABCAB";
+        String s2="AECB";
         int n=s1.length();
         int m=s2.length();
         int ans=common(s1,s2,s1.length(),s2.length());
-        // int dp[][]=new int [n+1][m+1];
-        // int ns=LCSdynamic(s1,s2,s1.length(),s2.length(),dp);
-        System.out.println(ans);
-        // System.out.println(ns);
+        int dp[][]=new int [n+1][m+1];
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                dp[i][j] = -1; // Initialize all values to -1
+            }
+        }
+        int ns=LCSdynamic(s1,s2,s1.length(),s2.length(),dp);
+        System.out.println();
+        System.out.println("RECURSION "+ ans);
+        System.out.println("DYNAMIC "+ns);
     }
 }
